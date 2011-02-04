06138473bb21912bc096b5ada0c7f62c9c130ce6
Manuel.tscholl <Manuel.tscholl@d82675b3-9a0f-2c67-7a7e-475c90f298e8>
2011-02-04 16:47:03 +0000

    Spezial Paddle Event

---
+++ KuBatsch/src/at/kubatsch/util/PaddleEventArgs.java
@@ -0,0 +1,30 @@
+/**
+ * author: Manuel Tscholl(mts3970)
+ * created on: 04.02.2011
+ * filename: PaddleEventArgs.java
+ * project: KuBaTsch
+ */
+package at.kubatsch.util;
+
+import at.kubatsch.model.Paddle;
+
+/**
+ * @author Manuel Tscholl (mts3970)
+ *
+ */
+public class PaddleEventArgs extends EventArgs
+{
+    private Paddle _paddle;
+
+    /**
+     * Initializes a new instance of the {@link PaddleEventArgs} class.
+     * @param paddle
+     */
+    public PaddleEventArgs(Paddle paddle)
+    {
+        super();
+        _paddle = paddle;
+    }
+    
+    
+}
